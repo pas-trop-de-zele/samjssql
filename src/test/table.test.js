67386600080ee2c9a0441f0e36f14eef7323686d
samle <samle@palantir.com>
2023-10-03 14:07:41 -0700

    Fix test to compare Table object

--- src/test/table.test.js
+++ src/test/table.test.js
@@ -86,11 +86,11 @@ describe('Table Class Unit Tests', () => {
             const tableA = new Table(A);
             const tableA1 = new Table(A1);
             const unionAB = tableA.union(tableA1);
-            expect(unionAB).toEqual({
+            expect(unionAB).toEqual(new Table({
                 name: ['John', 'Jane', 'Bob', 'Alice', 'John', 'Jane'],
                 age: [25, 30, 40, 35, 25, 30],
                 occupation: ['Software Engineer', 'Product Manager', 'Marketing Manager', 'Data Scientist', 'Software Engineer', 'Product Manager'],
-            });
+            }));
         });
         it('should throw error when there is a schema mismatch', () => {
             const tableA = new Table(A);
@@ -106,23 +106,23 @@ describe('Table Class Unit Tests', () => {
     describe('select', () => {
         it('should return a new table with only the selected columns', () => {
             const table = new Table(A);
-            expect(table.select('name', 'age')).toEqual({
+            expect(table.select('name', 'age')).toEqual(new Table({
                 name: ['John', 'Jane', 'Bob', 'Alice'],
                 age: [25, 30, 40, 35]
-            });
-            expect(table.select('name')).toEqual({
+            }));
+            expect(table.select('name')).toEqual(new Table({
                 name: ['John', 'Jane', 'Bob', 'Alice'],
-            });
+            }));
             const emptyTable = new Table(C);
-            expect(emptyTable.select()).toEqual({});
+            expect(emptyTable.select()).toEqual(new Table({}));
         });
         it('should return every column if specify no column', () => {
             const table = new Table(A);
-            expect(table.select()).toEqual({
+            expect(table.select()).toEqual(new Table({
                 name: ['John', 'Jane', 'Bob', 'Alice'],
                 age: [25, 30, 40, 35],
                 occupation: ['Software Engineer', 'Product Manager', 'Marketing Manager', 'Data Scientist']
-            });
+            }));
         });
 
         it('should throw an error if a selected column does not exist', () => {
